15cca4eddae8efbe0769f8d45875538a4249862f
IGul <aigul.zaripbekova@iaau.edu.kg>
2019-12-03 02:39:12 +0100

    BudgetCntrl, UIContrl and appCntrl is constructed

---
+++ app.js
@@ -0,0 +1,22 @@
+
+// BUDGET CONTROLLER
+let budgetController = (() => {
+
+})();
+
+budgetController.testing()
+
+// UI CONTROLLER
+let UIController = (() => {
+
+})();
+
+// GLOBAL APP CONTROLLER
+let controller = ((budgetCntrl, UICntrl) => {
+	
+})(budgetController, UIController);
+
+controller.init();
+
+
+
